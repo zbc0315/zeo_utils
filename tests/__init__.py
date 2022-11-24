64af1a9440e58ed10c10bc3dfa97ac8b349fe290
zhang <zhangbc0315@outlook.com>
2022-11-24 15:43:16 +0800

    test: test calculators of network

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 11/24/2022 10:49 AM
+# @Author  : zhangbc0315@outlook.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
+if __name__ == "__main__":
+    pass
